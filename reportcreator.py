0acc34da2dc1d817f032cdd3ce2240694ad71673
Rillatile <roman.malinkov@list.ru>
2020-06-03 22:39:36 +0700

    WIP 16: улучшена генерация отчёта, добавлена поддержка кириллицы

--- reportcreator.py
+++ reportcreator.py
@@ -4,7 +4,11 @@ from reportlab.lib import pdfencrypt
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import Paragraph, Frame
+from reportlab.pdfbase import ttfonts
+from reportlab.pdfbase import pdfmetrics
 from PySide2.QtWidgets import QFileDialog
+from PySide2.QtCore import QSysInfo
+from datetime import datetime
 
 
 # Функция генерации отчёта
@@ -13,29 +17,35 @@ def generate_report(module_name, module_success, result, message, parent=None):
     if directory != '':
         enc = pdfencrypt.StandardEncryption('3s7cmSzfMT', canModify=0)
         p = canvas.Canvas(f'{directory}/{module_name}_report.pdf', encrypt=enc)
-        p.setFont('Times-Bold', 18)
+        pdfmetrics.registerFont(ttfonts.TTFont('TimesNewRoman', 'times.ttf'))
+        pdfmetrics.registerFont(ttfonts.TTFont('TimesNewRomanBold', 'timesbd.ttf'))
         p.setTitle(f'{module_name}_report')
-        main_header_style = ParagraphStyle('header_style', fontName='Times-Bold', fontSize=18,
+        main_header_style = ParagraphStyle('header_style', fontName='TimesNewRomanBold', fontSize=18,
                                            alignment=1, spaceAfter=9, spaceBefore=9)
-        block_header_style = ParagraphStyle('header_style', fontName='Times-Bold', fontSize=14,
+        block_header_style = ParagraphStyle('header_style', fontName='TimesNewRomanBold', fontSize=14,
                                             alignment=1, spaceAfter=9, spaceBefore=9)
-        normal_style = ParagraphStyle('normal_style', fontName='Times-Roman', fontSize=14,
+        normal_style = ParagraphStyle('normal_style', fontName='TimesNewRoman', fontSize=14,
                                       alignment=1, spaceAfter=9, spaceBefore=9)
-        success_normal_style = ParagraphStyle('success_normal_style', fontName='Times-Roman',
+        success_normal_style = ParagraphStyle('success_normal_style', fontName='TimesNewRoman',
                                               fontSize=14, textColor=Color(0, 255, 0, 1),
                                               alignment=1, spaceAfter=9, spaceBefore=9)
-        failure_normal_style = ParagraphStyle('failure_normal_style', fontName='Times-Roman',
+        failure_normal_style = ParagraphStyle('failure_normal_style', fontName='TimesNewRoman',
                                               fontSize=14, textColor=Color(255, 0, 0, 1),
                                               alignment=1, spaceAfter=9, spaceBefore=9)
-        story = [Paragraph(f'"{module_name}" report', main_header_style)]
+        story = [
+            Paragraph(f'Отчёт о выполнении модуля "{module_name}"', main_header_style),
+            Paragraph(f'Операционная система: {QSysInfo.prettyProductName()}.', normal_style),
+            Paragraph(f'Дата и время генерации отчёта: {datetime.now().strftime("%d.%m.%Y %H:%M:%S.")}',
+                      normal_style)
+        ]
         if module_success == 'True':
-            story.append(Paragraph('Success', success_normal_style))
+            story.append(Paragraph(message, success_normal_style))
         else:
-            story.append(Paragraph('Failure', failure_normal_style))
+            story.append(Paragraph(message, failure_normal_style))
         for i in range(0, len(result)):
-            story.append(Paragraph(f'Block {i + 1}', block_header_style))
+            story.append(Paragraph(f'Блок №{i + 1}', block_header_style))
             for j in range(0, len(result[i]) - 1):
-                story.append(Paragraph(f'Command {j + 1} result:', normal_style))
+                story.append(Paragraph(f'Результат выполнения команды №{j + 1}:', normal_style))
                 if result[i][j][0] == 0:
                     story.append(Paragraph(f'{result[i][j][1].strip()}', success_normal_style))
                 else:
